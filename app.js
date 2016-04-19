b604af00061f32dc29f6b8b8508d33b3d87cdd5e
HiroNonoyama <h.nonoyama.117@gmail.com>
2016-04-19 23:20:28 +0900

    [REMOVE] view/index.jade
    [ADD] bundle.js & index.jade

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.set('view engine', 'jade');
 
-app.set('views', __dirname+'/views');
+app.set('views', __dirname);
 
 app.get('/', function (req, res) {
     res.render('index');
