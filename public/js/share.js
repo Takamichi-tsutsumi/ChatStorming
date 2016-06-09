b70325aeb92a6467a8a869cd722d5bde7fc66c3e
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2016-06-10 02:51:22 +0900

    [Add] share

--- public/js/share.js
+++ public/js/share.js
@@ -1,5 +1,7 @@
 $(function () {
-  hsize = $(window).height();
+  var hsize = $(window).height();
+  $(".home_container").css("height", hsize + "px");
+  $(".history").css("height", hsize - 320 + "px");
   $(".sub2").css("height", hsize - 40 + "px");
   $(".main").css("height", hsize - 120 + "px");
   $("a li").mouseover(function(){
