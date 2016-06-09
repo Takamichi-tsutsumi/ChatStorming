9e55222b2e8ce1e8848a70dc3f734152755b9fb1
HiroNonoyama <h.nonoyama.117@gmail.com>
2016-06-09 12:51:55 +0900

    [Add]NodeItem.jsx
    [Mod]NodeList.jsx

---
+++ src/components/NodeItem.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class NodeItem extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			name: props.nodeitem
+		}
+
+	}
+
+	render() {
+		return (
+			<div
+			onClick={() => {this.props.selected_change(this.state.name)}}
+			className={this.props.className}
+			>
+        { this.state.name }
+			</div>
+		)
+	}
+}
+
+export default NodeItem;
--- src/components/NodeList.jsx
+++ src/components/NodeList.jsx
@@ -1,14 +1,52 @@
 import React, { Component } from 'react';
+import 'axios';
 
+import NodeItem from './NodeItem.jsx';
 
 export class NodeList extends Component {
-    constructor(props) {
-        super(props)
-    }
 
-    render() {
-        return(
-            <div>This is Node list</div>
-        )
-    }
+  componentDidMount() {
+    //const node = axios.get('URL');
+    const node = ["node1", "node2"]
+    this.setState({node: node});
+  }
+
+  deleteSelected(mapNodeName) {
+    const node = this.state.node
+    updated_node = node.slice(node.indexOf(mapNodeName), 1)
+    this.setState({node: updated_node, selected: ""})
+  }
+
+  NodeItems() {
+    if (this.state.node.length === 0) return () => {return ""};
+    return this.state.node.map((nodeitem) => {
+          return (
+            <NodeItem
+            className={ this.state.selected == nodeitem ? "selected" : "" }
+            key={nodeitem}
+            nodeitem={nodeitem}
+            selected_change={(nodeName) => {
+              this.setState({selected: nodeName});
+              window.selected = nodeName;
+            }}
+             />
+          )
+    })
+  }
+
+  constructor(props) {
+      super(props)
+
+      this.state = { node: [], selected: "" };
+      this.NodeItems = this.NodeItems.bind(this)
+  }
+
+
+  render() {
+      return(
+          <ul>
+            {this.NodeItems()}
+          </ul>
+      )
+  }
 }
